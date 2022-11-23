da87ef83bc4bf7bc00dff968fed4b6eaa6438c52
jack <1689798397@qq.com>
2022-11-23 10:04:28 +0800

    加入img标签的转换

--- switch_pic.py
+++ switch_pic.py
@@ -72,26 +72,38 @@ def base64_net(base64str):
         return "![](" + pic + ")"
 
 
+# 绝对路径转相对路径
+def disk_disk(md_dir_path, source_pic):
+    pic_name = str(uuid.uuid4()) + ".png"
+    pic = md_dir_path + "/media/" + pic_name
+    if not os.path.exists(md_dir_path + "/media"):
+        os.makedirs(md_dir_path + "/media")
+    with open(source_pic, "rb") as r:
+        with open(pic, "wb") as w:
+            w.write(r.read())
+    return "![](./media/" + pic_name + ")"
+
+
 # 网络图片转化为磁盘图片
-def net_disk(pic_path, pic_url):
+def net_disk(md_dir_path, pic_url):
     proxies = {"http": config["proxy"], "https": config["proxy"]}
     pic_data = requests.get(url=pic_url, proxies=proxies).content
     pic_name = str(uuid.uuid4()) + ".png"
-    pic = pic_path + "/media/" + pic_name
-    if not os.path.exists(pic_path + "/media"):
-        os.makedirs(pic_path + "/media")
+    pic = md_dir_path + "/media/" + pic_name
+    if not os.path.exists(md_dir_path + "/media"):
+        os.makedirs(md_dir_path + "/media")
     with open(pic, "wb") as f:
         f.write(pic_data)
     return "![](./media/" + pic_name + ")"
 
 
 # base64转换为磁盘图片
-def base64_disk(pic_path, base64str):
+def base64_disk(md_dir_path, base64str):
     base64str = base64str.replace("data:image/png;base64,", "")
     pic_name = str(uuid.uuid4()) + ".png"
-    pic = pic_path + "/media/" + pic_name
-    if not os.path.exists(pic_path + "/media"):
-        os.makedirs(pic_path + "/media")
+    pic = md_dir_path + "/media/" + pic_name
+    if not os.path.exists(md_dir_path + "/media"):
+        os.makedirs(md_dir_path + "/media")
     with open(pic, "wb") as f:
         f.write(base64.b64decode(base64str))
     return "![](./media/" + pic_name + ")"
@@ -106,33 +118,45 @@ def read_md(md_absolute_path, target):
     md_str = ""
     with open(md_absolute_path, "r", encoding='UTF-8') as f:
         md_str = f.read()
-        # 正则匹配所有图片标签
-        pic_label_list = re.findall(r'!\[.*\]\(.*\)', md_str)
+        # 正则匹配所有markdown图片标签
+        img_label_list = re.findall(r'!\[.*\]\(.*\)', md_str)
+        # 正则匹配所有html图片标签
+        html_label_list = re.findall(r'<img.*>?', md_str)
+        pic_label_list = img_label_list + html_label_list
         # 遍历图片标签
         for pic_label in pic_label_list:
+            pic_path = ""
             # 获取图片路径
-            pic_path = re.search(r'\(.*\)', pic_label).group().replace("(", "").replace(")", "")
+            if "![" in pic_label:
+                pic_path = re.search(r'\(.*\)', pic_label).group().replace("(", "").replace(")", "")
+            elif "<img" in pic_label:
+                pic_path = re.search(r'src=\"(.*?)\"', pic_label).group().replace('src="', "").replace('"', "")
             # 新图片地址
             new_pic_label = ""
-
             # 如果是网络图片
             if "http" in pic_path:
                 if target == "d":
                     new_pic_label = net_disk(md_dirname, pic_path)
                 if target == "b":
                     new_pic_label = net_base64(pic_path)
+                if target == "n":
+                    new_pic_label = pic_label
             # 如果是base64
             elif "data:image" in pic_path:
                 if target == "d":
                     new_pic_label = base64_disk(md_dirname, pic_path)
                 if target == "n":
                     new_pic_label = base64_net(pic_path)
+                if target == "b":
+                    new_pic_label = pic_label
             # 如果是绝对路径
             elif os.path.exists(pic_path):
                 if target == "b":
                     new_pic_label = disk_base64(pic_path)
                 if target == "n":
                     new_pic_label = disk_net(pic_path)
+                if target == "d":
+                    new_pic_label = disk_disk(md_dirname, pic_path)
             # 如果是相对路径，拼接得到绝对路径
             elif "./" in pic_path:
                 pic_abs_path = md_dirname + (pic_path.replace("./", "/"))
@@ -140,6 +164,8 @@ def read_md(md_absolute_path, target):
                     new_pic_label = disk_base64(pic_abs_path)
                 if target == "n":
                     new_pic_label = disk_net(pic_abs_path)
+                if target == "d":
+                    new_pic_label = pic_label
             else:
                 pic_abs_path = md_dirname + "/" + pic_path
                 if os.path.exists(pic_abs_path):
@@ -147,6 +173,8 @@ def read_md(md_absolute_path, target):
                         new_pic_label = disk_base64(pic_abs_path)
                     if target == "n":
                         new_pic_label = disk_net(pic_abs_path)
+                    if target == "d":
+                        new_pic_label = pic_label
             md_str = md_str.replace(pic_label, new_pic_label)
     with open(md_absolute_path, "w", encoding='UTF-8') as w:
         w.write(md_str)
