a12ae3cb3a7076aa45ee15a28d2a524b1e03562b
jack <1689798397@qq.com>
2022-11-24 21:43:57 +0800

    优化正则匹配失败的情况

--- switch_pic.py
+++ switch_pic.py
@@ -2,7 +2,6 @@ import base64
 import glob
 import os
 import re
-import sys
 import uuid
 
 import requests
@@ -14,21 +13,29 @@ config = {
 
 # 磁盘中的图片转换为base64
 def disk_base64(pic_path):
-    with open(pic_path, "rb") as pic_b:
-        base64_data = base64.b64encode(pic_b.read())
-        base64_str = base64_data.decode()
-        pic = "data:image/png;base64,%s" % base64_str
-        return "![](" + pic + ")"
+    try:
+        with open(pic_path, "rb") as pic_b:
+            base64_data = base64.b64encode(pic_b.read())
+            base64_str = base64_data.decode()
+            pic = "data:image/png;base64,%s" % base64_str
+            return "![](" + pic + ")"
+    except:
+        print(pic_path + " 转化失败！")
+        return "![](" + pic_path + ")"
 
 
 # 网络图片转换为base64
 def net_base64(pic_url):
-    proxies = {"http": config["proxy"], "https": config["proxy"]}
-    result_data = requests.get(url=pic_url, proxies=proxies).content
-    base64_data = base64.b64encode(result_data)
-    base64_str = base64_data.decode()
-    pic = "data:image/png;base64,%s" % base64_str
-    return "![](" + pic + ")"
+    try:
+        proxies = {"http": config["proxy"], "https": config["proxy"]}
+        result_data = requests.get(url=pic_url, proxies=proxies).content
+        base64_data = base64.b64encode(result_data)
+        base64_str = base64_data.decode()
+        pic = "data:image/png;base64,%s" % base64_str
+        return "![](" + pic + ")"
+    except:
+        print(pic_url + " 转化失败！")
+        return "![](" + pic_url + ")"
 
 
 # 磁盘图片上传到网络
@@ -52,61 +59,77 @@ def disk_net(pic_path):
 
 # base64上传网络
 def base64_net(base64str):
-    base64str = base64str.replace("data:image/png;base64,", "")
-    url = "http://127.0.0.1:36677/upload"
-    with open("D:\\temp.png", "wb") as w:
-        w.write(base64.b64decode(base64str))
-    pic_list = ["D:\\temp.png"]
-    requests_body = {
-        "list": pic_list
-    }
-    result_json = requests.post(url, json=requests_body).json()
-    # 判断上传是否成功
-    if result_json['success']:
-        pic = result_json['result'][0]
-        return "![](" + pic + ")"
-    else:
-        with open("./error.log", "a+", encoding='UTF-8') as f:
-            f.write("\n" + base64str)
-        pic = base64str
-        return "![](" + pic + ")"
+    try:
+        base64str = base64str.replace("data:image/png;base64,", "")
+        url = "http://127.0.0.1:36677/upload"
+        with open("D:\\temp.png", "wb") as w:
+            w.write(base64.b64decode(base64str))
+        pic_list = ["D:\\temp.png"]
+        requests_body = {
+            "list": pic_list
+        }
+        result_json = requests.post(url, json=requests_body).json()
+        # 判断上传是否成功
+        if result_json['success']:
+            pic = result_json['result'][0]
+            return "![](" + pic + ")"
+        else:
+            with open("./error.log", "a+", encoding='UTF-8') as f:
+                f.write("\n" + base64str)
+            pic = base64str
+            return "![](" + pic + ")"
+    except:
+        print(base64str + " 转化失败！")
+        return "![](" + base64str + ")"
 
 
 # 绝对路径转相对路径
 def disk_disk(md_dir_path, source_pic):
-    pic_name = str(uuid.uuid4()) + ".png"
-    pic = md_dir_path + "/media/" + pic_name
-    if not os.path.exists(md_dir_path + "/media"):
-        os.makedirs(md_dir_path + "/media")
-    with open(source_pic, "rb") as r:
-        with open(pic, "wb") as w:
-            w.write(r.read())
-    return "![](./media/" + pic_name + ")"
+    try:
+        pic_name = str(uuid.uuid4()) + ".png"
+        pic = md_dir_path + "/media/" + pic_name
+        if not os.path.exists(md_dir_path + "/media"):
+            os.makedirs(md_dir_path + "/media")
+        with open(source_pic, "rb") as r:
+            with open(pic, "wb") as w:
+                w.write(r.read())
+        return "![](./media/" + pic_name + ")"
+    except:
+        print(source_pic + " 转化失败！")
+        return "![](" + source_pic + ")"
 
 
 # 网络图片转化为磁盘图片
 def net_disk(md_dir_path, pic_url):
-    proxies = {"http": config["proxy"], "https": config["proxy"]}
-    pic_data = requests.get(url=pic_url, proxies=proxies).content
-    pic_name = str(uuid.uuid4()) + ".png"
-    pic = md_dir_path + "/media/" + pic_name
-    if not os.path.exists(md_dir_path + "/media"):
-        os.makedirs(md_dir_path + "/media")
-    with open(pic, "wb") as f:
-        f.write(pic_data)
-    return "![](./media/" + pic_name + ")"
+    try:
+        proxies = {"http": config["proxy"], "https": config["proxy"]}
+        pic_data = requests.get(url=pic_url, proxies=proxies).content
+        pic_name = str(uuid.uuid4()) + ".png"
+        pic = md_dir_path + "/media/" + pic_name
+        if not os.path.exists(md_dir_path + "/media"):
+            os.makedirs(md_dir_path + "/media")
+        with open(pic, "wb") as f:
+            f.write(pic_data)
+        return "![](./media/" + pic_name + ")"
+    except:
+        print(pic_url + " 转化失败！")
+        return "![](" + pic_url + ")"
 
 
 # base64转换为磁盘图片
 def base64_disk(md_dir_path, base64str):
-    base64str = base64str.replace("data:image/png;base64,", "")
-    pic_name = str(uuid.uuid4()) + ".png"
-    pic = md_dir_path + "/media/" + pic_name
-    if not os.path.exists(md_dir_path + "/media"):
-        os.makedirs(md_dir_path + "/media")
-    with open(pic, "wb") as f:
-        f.write(base64.b64decode(base64str))
-    return "![](./media/" + pic_name + ")"
+    try:
+        base64str = base64str.replace("data:image/png;base64,", "")
+        pic_name = str(uuid.uuid4()) + ".png"
+        pic = md_dir_path + "/media/" + pic_name
+        if not os.path.exists(md_dir_path + "/media"):
+            os.makedirs(md_dir_path + "/media")
+        with open(pic, "wb") as f:
+            f.write(base64.b64decode(base64str))
+        return "![](./media/" + pic_name + ")"
+    except:
+        print(base64str + " 转化失败！")
+        return "![](" + base64str + ")"
 
 
 # 修改md文件
@@ -128,9 +151,17 @@ def read_md(md_absolute_path, target):
             pic_path = ""
             # 获取图片路径
             if "![" in pic_label:
-                pic_path = re.search(r'\(.*\)', pic_label).group().replace("(", "").replace(")", "")
+                pic_path = re.search(r'\(.*\)', pic_label)
+                if pic_path:
+                    pic_path = pic_path.group().replace("(", "").replace(")", "")
+                else:
+                    continue
             elif "<img" in pic_label:
-                pic_path = re.search(r'src=\"(.*?)\"', pic_label).group().replace('src="', "").replace('"', "")
+                pic_path = re.search(r'src=\"(.*?)\"', pic_label)
+                if pic_path:
+                    pic_path = pic_path.group().replace('src="', "").replace('"', "")
+                else:
+                    continue
             # 新图片地址
             new_pic_label = ""
             # 如果是网络图片
